811f0060e6cb72048c93c814d9afa2fc928064ee
Miguel Angel Aguilar Garcia <migue_ag18@hotmail.com>
2021-03-17 22:04:04 -0600

    haciendo las funciones

--- app.js
+++ app.js
@@ -1,72 +1,56 @@
-const AWG = [18, 16, 14, 12, 10, 8, 6];
+const AWG =  ['14', '12', '10', '8', '6', '4', '3', '2', '1', '1/0', '2/0', '3/0', '4/0', '250', '300', '350', '400', '500', '600', '700', '750', '800', '900', '1000', '1250', '1500', '1750', '2000'];
+const mm2 = [2.08, 3.31, 5.26, 8.37, 13.3, 21.2, 26.7, 33.6, 42.4, 53.49, 67.43, 85.01, 107.2, 127, 152.0, 177, 203, 253, 304, 355, 380, 405, 456, 507, 633, 700, 887, 1013];
+
+const tableFactorGrouping = {3:1, 6:0.8, 9:0.7, 20:0.5, 30:0.45, 40:0.4, 50:0.35};
+
+const cooper60 = [15, 20, 30, 40, 55, 70, 85, 90, 110, 125, 145, 165, 195, 215, 240, 260, 280, 320, 350, 385, 400, 410, 435, 455, 495, 525, 545, 555];
+const cooper75 = [20, 25, 35, 50, 65, 85, 100, 115, 130, 150, 175, 200, 230, 255, 285, 310, 335, 380, 420, 460, 475, 490, 520, 545, 590, 625, 650, 665];
+const cooper90 = [25, 30, 40, 55, 75, 95, 115, 130, 145, 170, 195, 225, 260, 290, 320, 350, 380, 430, 475, 520, 535, 555, 585, 615, 665, 705, 735, 750];
+
+const aluminium60 = [0, 15, 25, 35, 40, 55, 65, 75, 85, 100, 115, 130, 150, 170, 195, 210, 225, 260, 285, 315, 320, 330, 355, 375, 405, 435, 455, 470];
+const aluminium75 = [0, 20, 30, 49, 50, 65, 75, 90, 100, 120, 135, 155, 180, 205, 230, 250, 270, 310, 340, 375, 385, 395, 425, 445, 485, 520, 545, 560];
+const aluminium90 = [0, 25, 35, 45, 55, 75, 85, 100, 115, 135, 150, 175, 205, 230, 260, 280, 305, 350, 385, 425, 435, 445, 480, 500, 545, 585, 615, 630];
 
 window.addEventListener("DOMContentLoaded", calc_main, false);
 document.getElementById("form0").addEventListener("change", calc_main);
-document.getElementById("form").addEventListener("change", calc_main);
+document.getElementById("form1").addEventListener("change", calc_main);
+document.getElementById("form2").addEventListener("change", calc_main);
+document.getElementById("form3").addEventListener("change", calc_main);
+document.getElementById("form4").addEventListener("change", calc_main);
+document.getElementById("form5").addEventListener("change", calc_main);
+document.getElementById("formA").addEventListener("change", calc_main);
+document.getElementById("formB").addEventListener("change", calc_main);
 
 function calc_main() {
     console.log("cambio");
 
     /*data */
+    /*form0 */
     var decimals = Number.parseInt(document.getElementById("decimals").value);
-
-    for(let ii = 0; ii < 7; ii++) {
-        document.getElementById("item5AWG" + String(AWG[ii])).value = "0";
-        document.getElementById("item6AWG" + String(AWG[ii])).value = "0";
-        document.getElementById("item7AWG" + String(AWG[ii])).value = "0";
-    }
-
-    var [numAWG, numAWGTotal] = funNum();
-    
-    var inAWG = [];
-    for(let ii = 18; ii >= 6; ii = ii-2) {
-
-        inAWG.push(Number.parseFloat(document.getElementById("inAWG" + String(ii)).innerHTML));
-    }
-    console.log(inAWG);
-
-    var cmAWG = [];
-    for(let ii = 18; ii >= 6; ii = ii-2) {
-
-        cmAWG.push(Number.parseFloat(document.getElementById("cmAWG" + String(ii)).innerHTML));
-    }
-    console.log(cmAWG);
+    /*form1 */
+    var system = document.getElementById("system").value;
+    var voltage = Number.parseFloat(document.getElementById("voltage").value);
+    var conductorsPerPhase = Number.parseInt(document.getElementById("conductorsPerPhase").value);
+    /*form2 */
+    var current = Number.parseFloat(document.getElementById("current").value);
+    var realPower = Number.parseFloat(document.getElementById("realPower").value);
+    var apparentPower = Number.parseFloat(document.getElementById("apparentPower").value);
+    var pf = Number.parseFloat(document.getElementById("pf").value);
+    /*form3 */
+    var lenght = Number.parseFloat(document.getElementById("lenght").value);
+    var voltageDropPercent = Number.parseFloat(document.getElementById("realPower").value);
+    var voltageDropVolts = Number.parseFloat(document.getElementById("apparentPower").value);
+    /*form4 */
+    var Tambient = Number.parseFloat(document.getElementById("Tambient").value);
+    var Tinsulation = Number.parseInt(document.getElementById("Tinsulation").value);
+    var conductorMaterial = document.getElementById("conductorMaterial").value;
+    var racewayMaterial = document.getElementById("racewayMaterial").value;
+    /*formA */
+    var conductorsPerConduit = Number.parseInt(document.getElementById("conductorsPerConduit").value);
 
     /*computation */
-    returnfunCheckbox = funCheckbox(numAWGTotal);
+    var current = current(numAWGTotal);
     console.log(returnfunCheckbox);
-
-    if (returnfunCheckbox === true) {
-        var [numAWG, numAWGTotal] = funNum();
-    }
-
-    var inAWGTotal = [];
-    for(let ii = 0; ii < 7; ii++) {
-
-        inAWGTotal.push(numAWGTotal[ii]*inAWG[ii]);
-    }
-    console.log(inAWGTotal);
-
-    var cmAWGTotal = [];
-    for(let ii = 0; ii < 7; ii++) {
-
-        cmAWGTotal.push(numAWGTotal[ii]*cmAWG[ii]);
-    }
-    console.log(cmAWGTotal);
-
-    var inTotal = 0;
-    for(let ii = 0; ii < 7; ii++) {
-
-        inTotal = inTotal + inAWGTotal[ii];
-    }
-    console.log(inTotal);
-
-    var cmTotal = 0;
-    for(let ii = 0; ii < 7; ii++) {
-
-        cmTotal = cmTotal + cmAWGTotal[ii];
-    }
-    console.log(cmTotal);
 
     /*results */
     let e = 0;
@@ -112,71 +96,43 @@ function funNum() {
     return [numAWG, numAWGTotal];
 }
 
-function funCheckbox(numAWGTotal) {
-    var clamp = document.getElementById("clamp").checked;
-    var egc = document.getElementById("egc").checked;
-    var iegc = document.getElementById("iegc").checked;
 
-    var clamp_index = -1;
-    var egc_index = -1;
-    var iegc_index = -1;
+function current(system, powerReal, pf) {
 
-    if (clamp === true) {
-        
-        for(let ii = 0; ii < 7; ii++) {
-            
-            if (numAWGTotal[ii] > 0){
-                clamp_index = ii;
-            }
-        }
-        console.log(clamp_index);
-
-        if (clamp_index >= 0){
-            for(let ii = 0; ii < 7; ii++) {
-                document.getElementById("item5AWG" + String(AWG[ii])).value = "0";
-            }
-            document.getElementById("item5AWG" + String(AWG[clamp_index])).value = "1";
-        }
+    let current;
+    if (system === "single"){
+        current = powerReal/(voltage*pf)
     }
-    if (egc === true) {
-
-        for(let ii = 0; ii < 7; ii++) {
-            
-            if (numAWGTotal[ii] > 0){
-                egc_index = ii;
-            }
-        }
-        console.log(egc_index);
-
-        if (egc_index >= 0){
-            for(let ii = 0; ii < 7; ii++) {
-                document.getElementById("item6AWG" + String(AWG[ii])).value = "0";
-            }
-            document.getElementById("item6AWG" + String(AWG[egc_index])).value = "1";
-        }
-    }
-    if (iegc === true) {
-
-        for(let ii = 0; ii < 7; ii++) {
-            
-            if (numAWGTotal[ii] > 0){
-                iegc_index = ii;
-            }
-        }
-        console.log(iegc_index);
-
-        if (iegc_index >= 0){
-            for(let ii = 0; ii < 7; ii++) {
-                document.getElementById("item7AWG" + String(AWG[ii])).value = "0";
-            }
-            document.getElementById("item7AWG" + String(AWG[iegc_index])).value = "1";
-        }
+    else if (system === 'three') {
+        current = powerReal/(Math.sqrt(3)*voltage*pf);
     }
 
-    if (clamp_index >= 0 || egc_index >= 0 || iegc_index >= 0) {
-        return true;
+    return current;
+}
+
+function RTemp(R, Tinsulation) {
+
+    let RTemp = R*(1 + 0.0039*(Tinsulation - 75));
+
+    return RTemp;
+}
+
+function Ze(RTemp, XL, pf) {
+
+    let Ze = (RTemp*pf + XL*Math.sin(Math.acos(pf)))/1000;
+
+    return RTemp;
+}
+
+function voltageDropPercent(system, Ze, lenght, current, voltage, conductorsPerPhase) {
+
+    let voltageDropPercent;
+    if (system === "single"){
+        voltageDropPercent = 2*Ze*lenght*current*100/(voltage*conductorsPerPhase);
     }
-    else {
-        return false;
+    else if (system === 'three') {
+        voltageDropPercent = Math.sqrt(3)*Ze*lenght*current*100/(voltage*conductorsPerPhase);
     }
+
+    return voltageDropPercent;
 }
